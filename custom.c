52c0d939d2e02bfc4bccf566bdf7e046cdb82e4a
Akinori MUSHA <knu@idaemons.org>
2012-06-30 18:15:50 +0900

    Avoid segv in saving the customizer results. [FDclone-users:00983]

--- custom.c
+++ custom.c
@@ -4004,11 +4004,10 @@ XFILE *fp;
 	char *new;
 	int i, j, n;
 
-	if (origflaglist) new = NULL;
+	if (origflaglist || origmaxfdtype <= 0) new = NULL;
 	else {
-		for (n = 0; n < origmaxfdtype; n++) /*EMPTY*/;
-		origflaglist = new = Xmalloc(n * sizeof(char));
-		memset(origflaglist, 0, n * sizeof(char));
+		origflaglist = new = Xmalloc(origmaxfdtype * sizeof(char));
+		memset(origflaglist, 0, origmaxfdtype * sizeof(char));
 	}
 
 	for (i = n = 0; i < maxfdtype; i++) {
